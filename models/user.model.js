e6d020ad7c7a80b2f6b05d1e8b7c58602272bba1
Pumpel <romanbaos@gmail.com>
2020-08-26 21:29:33 +0200

    added user model

---
+++ models/user.model.js
@@ -0,0 +1,84 @@
+const bcrypt = require("bcrypt")
+const mongoose = require("mongoose")
+
+const EMAIL_PATTERN = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+const generateRandomToken = () => {
+  const characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  let token = ""
+  for (let i = 0; i < 25; i++) {
+    token += characters[Math.floor(Math.random() * characters.length)]
+  }
+  return token
+}
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    minLength: [3, "Name needs at least 3 characters"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is require"],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [EMAIL_PATTERN, " Email is invalid"],
+  },
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    minlength: [8, "Password must be at least 8 characters long"],
+  },
+  pictures: {
+    type: [String],
+  },
+  description: {
+    type: String,
+    maxlength: 240,
+  },
+  activation: {
+    active: {
+      type: Boolean,
+      default: false,
+    },
+    token: {
+      type: String,
+      default: generateRandomToken,
+    },
+    profileFinished: {
+      type: String,
+      default: false,
+    },
+  },
+  social: {
+    google: String,
+  },
+})
+
+userSchema.pre("save", function (next) {
+  if (this.isModified("password")) {
+    bcrypt.hash(this.password, 10).then((hash) => {
+      this.password = hash
+      next()
+    })
+  } else {
+    next()
+  }
+})
+
+userSchema.methods.checkPassword = function (password) {
+  return bcrypt.compare(password, this.password)
+}
+
+const User = moongoose.model("User", userSchema)
+
+module.exports = User
